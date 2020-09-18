a8ab37e30b870312f7d228ac6914f811711e490b
Jennalyn Kabiling <heyimjennaa@hotmail.com>
2020-09-17 23:36:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -89,4 +89,4 @@ while True:
   print(user_food)
 
 #Goodbye for now
-print ("I think it is time for me to go ahead and make a meal! All that talk about food... Hehe, but it was so wonderful chatting with you! Hope you can visit my city sometime soon!")
+print ("I think it is time for me to go ahead and make a meal! All that talk about food... Hehe, but it was so wonderful chatting with you! I truly hope you can visit my city sometime soon!")
